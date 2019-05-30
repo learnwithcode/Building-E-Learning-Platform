0569aa6a59a39286e55472fafdeedeb75c19c556
Abhiraj Pundir <pundirabhiraj.com>
2019-05-30 15:21:14 +0530

    Restricting access to class-based views

--- courses/views.py
+++ courses/views.py
@@ -3,7 +3,8 @@ from django.urls import reverse_lazy
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView,\
 										DeleteView
-
+from django.contrib.auth.mixins import LoginRequiredMixin, \
+										PermissionRequiredMixin
 from .models import Course
 
 class OwnerMixin(object):
@@ -13,11 +14,13 @@ class OwnerMixin(object):
 
 class OwnerEditMixin(object):
 	def form_valid(self, form):
-	form.instance.owner = self.request.user
-	return super(OwnerEditMixin, self).form_valid(form)
+		form.instance.owner = self.request.user
+		return super(OwnerEditMixin, self).form_valid(form)
 
-class OwnerCourseMixin(OwnerMixin):
+class OwnerCourseMixin(OwnerMixin, LoginRequiredMixin):
 	model = Course
+	fields = ['subject', 'title', 'slug', 'overview']
+	success_url = reverse_lazy('manage_course_list')
 
 class OwnerCourseEditMixin(OwnerCourseMixin, OwnerEditMixin):
 	fields = ['subject', 'title', 'slug', 'overview']
@@ -27,12 +30,19 @@ class OwnerCourseEditMixin(OwnerCourseMixin, OwnerEditMixin):
 class ManageCourseListView(OwnerCourseMixin, ListView):
 	template_name = 'courses/manage/course/list.html'
 
-class CourseCreateView(OwnerCourseEditMixin, CreateView):
-	pass						
+class CourseCreateView(PermissionRequiredMixin,
+								OwnerCourseEditMixin, 
+								CreateView):
+	permission_required = 'courses.add_course'					
 
-class CourseUpdateView(OwnerCourseEditMixin, UpdateView):
-	pass
+class CourseUpdateView(PermissionRequiredMixin,
+								OwnerCourseEditMixin, 
+								UpdateView):
+	permission_required = 'courses.change_course' 
 
-class CourseDeleteView(OwnerCourseMixin, DeleteView):
+class CourseDeleteView(PermissionRequiredMixin,
+								OwnerCourseMixin, 
+								DeleteView):
 	template_name = 'courses/manage/course/delete.html'
 	success_url = reverse_lazy('manage_course_list')
+	permission_required = 'courses.delete_course'
